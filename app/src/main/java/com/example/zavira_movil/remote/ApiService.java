20cd1037856015e88a93479de9893aeef6e114d8
Daniel Diaz <carlosdanielpinzadiaz@gmail.com>
2025-09-27 11:42:32 -0500

    fix(ApiService): se corrigieron errores en los métodos y rutas de la interfaz, ajustando endpoints para login, envío de respuestas y manejo de fotos de perfil. Se eliminaron importaciones innecesarias y se mejoró la organización del código para mayor legibilidad.

--- app/src/main/java/com/example/zavira_movil/remote/ApiService.java
+++ app/src/main/java/com/example/zavira_movil/remote/ApiService.java
@@ -1,38 +1,70 @@
+package com.example.zavira_movil.remote;
+
+import com.example.zavira_movil.BasicResponse;
+import com.example.zavira_movil.PreguntaAcademica;
+import com.example.zavira_movil.QuizCerrarRequest;
+import com.example.zavira_movil.QuizInicialResponse;
+import com.example.zavira_movil.QuizResponse;
+import com.example.zavira_movil.model.Estudiante;
+import com.example.zavira_movil.model.KolbResultado;
+import com.example.zavira_movil.model.LoginRequest;
+
+import com.example.zavira_movil.model.KolbRequest;
+import com.example.zavira_movil.model.KolbResponse;
+import com.example.zavira_movil.model.PreguntasKolb;
+
+
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
 public interface ApiService {
 
+    //Logue Estudiante
     @POST("estudiante/login")
     Call<ResponseBody> loginEstudiante(@Body LoginRequest request);
-
     @GET("perfilEstudiante")
     Call<Estudiante> getPerfilEstudiante();
 
+    //Estilo de Kold
     @GET("kolb/preguntas")
     Call<List<PreguntasKolb>> getPreguntas();
-
     @POST("kolb/enviar")
     Call<KolbResponse> guardarRespuestas(@Body KolbRequest request);
-
     @GET("kolb/resultado")
     Call<KolbResultado> obtenerResultado();
 
-    // Métodos de quiz inicial
+    //Examen Inicial
     @POST("quizz/iniciar")
     Call<QuizInicialResponse> iniciar(
             @Header("Authorization") String bearerToken,
             @Body Map<String, Object> body
     );
-
     @POST("quiz-inicial/cerrar")
     Call<ResponseBody> cerrar(
             @Header("Authorization") String bearerToken,
             @Body QuizCerrarRequest request
     );
 
-    // Métodos para la foto de perfil
+    //MEtodos para la foto de perfil
     @Multipart
     @POST("users/me/photo")
     Call<ResponseBody> subirFoto(@Part MultipartBody.Part foto);
-
     @DELETE("users/me/photo")
     Call<ResponseBody> eliminarFoto();
+
+    //Test y Niveles por Area
+
+
+
 }
