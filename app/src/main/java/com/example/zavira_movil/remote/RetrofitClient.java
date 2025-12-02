eda220e76d57d2689cfd1cf99366473f0d2f3d60
Bryanhurtado0006 <bryandreshurtado18@gmail.com>
2025-12-02 13:49:31 -0500

    test: point BASE_URL to https://eduexce-backend.ddns.net/ for testing

--- app/src/main/java/com/example/zavira_movil/remote/RetrofitClient.java
+++ app/src/main/java/com/example/zavira_movil/remote/RetrofitClient.java
@@ -16,7 +16,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public final class RetrofitClient {
 
     //  URL NGROK ACTUALIZADA (con soporte OpenAI/IA)
-    private static final String BASE_URL = "https://churnable-nimbly-norbert.ngrok-free.dev";
+    private static final String BASE_URL = "https://eduexce-backend.ddns.net/";
 
     // DEBUG LOCAL (comentado hasta resolver firewall):
     // private static final String BASE_URL = "http://192.168.1.5:3333/";
